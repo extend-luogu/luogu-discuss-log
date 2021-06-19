8748326afb137b08db721c12aeedebb2daa560f4
bossbaby <3064273390@qq.com>
2021-06-19 14:34:27 +0800

    add feature: clean database

---
+++ clean.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('config.php');
+
+if (!isset($_GET['pass']) || $_GET['pass'] != $admin_pass) die('require pass');
+
+$min_clk = $_GET['click'];
+if (!isset($_GET['click'])) $min_clk = 10;
+
+$link->query("delete from discuss_log where exists (select thread from discuss_count where click < $min_clk and discuss_log.thread = discuss_count.thread)");
