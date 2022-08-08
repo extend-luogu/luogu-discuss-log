b53c25be4100dd31e45ed4372bda1db4ad706912
bossbaby2005 <34993620+bossbaby2005@users.noreply.github.com>
2022-08-08 18:12:07 +0800

    Update default_config.php

--- default_config.php
+++ default_config.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+  things need modifying:
+  admin password (LINE 13)
+  starting thread (LINE 14)
+  cookie (LINE 16)
+  database connection (LINE 21)
+*/
+
 ini_set("display_errors", "0");
 
 $admin_pass = 'your pass';
@@ -7,7 +15,7 @@ $st_thread = 0; // thread less than this wouldn't be recorded
 
 $cookie = '__client_id=<your cid>;_uid=<your uid>;';
 
-$announce_ver = 1;
+$announce_ver = 1; // update this each time you update the announcement. this is used to control cache policy
 
 $start = microtime(true); // to get the time of rendering
 $link = new mysqli("localhost", "your username", "your password", "your database name"); // init db connection
